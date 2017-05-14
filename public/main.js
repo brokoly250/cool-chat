c997618fdae360ff6fb531634904715faf873b2f
jesus <broly_freestyle@hotmail.com>
2017-05-14 18:52:33 +0200

    Adicion de privados y otras mejoras esteticas, con URL pruebas

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-const socket = io.connect('http://cool-chat-cool-chat.1d35.starter-us-east-1.openshiftapps.com:8080', {'forceNew': true})
+const socket = io.connect('http://cool-chat-cool-chat.1d35.starter-us-east-1.openshiftapps.com', {'forceNew': true})
 var nickname = "";
 var contador = "";
 //var sin_leer = [];
